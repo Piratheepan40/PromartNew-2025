3f485a424b752f3843838d760aa402a79ffee7a9
Ainkaran01 <kirubakaranainkaran@gmail.com>
2025-12-24 17:11:07 +0530

    bug

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/db.js";
 import bcrypt from "bcryptjs";
@@ -75,43 +74,3 @@ User.init(
 );
 
 export default User;
-=======
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-
-const userSchema = new mongoose.Schema(
-  {
-    companyName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    password: { type: String, required: true },
-    verified: { type: Boolean, default: false },
-
-    // 🧩 Add user role
-    role: {
-      type: String,
-      enum: ["company", "admin"],
-      default: "company",
-    },
-    passwordChangedAt: Date,
-    isActive: { type: Boolean, default: true },
-  },
-  { timestamps: true }
-);
-
-// 🔒 Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// 🔐 Compare passwords
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-export default User;
->>>>>>> f8244c1 (admin  code added)
